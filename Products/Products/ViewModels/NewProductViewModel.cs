bc7b5f2c8bf166974b9c9f040cbe0c97d1693c27
cherrera <cherrera@chejconsultor.com.ve>
2017-09-30 14:17:30 -0400

    Sincronizacion - 30/09/2017 - 02:17 PM

--- Products/Products/ViewModels/NewProductViewModel.cs
+++ Products/Products/ViewModels/NewProductViewModel.cs
@@ -1,9 +1,78 @@
 ﻿namespace Products.ViewModels
 {
-    public class NewProductViewModel
+    using System.ComponentModel;
+    using System;
+
+    public class NewProductViewModel : INotifyPropertyChanged
     {
+
+        #region Attributes
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string _description;
+        private string _price;
+        private bool _isToggled;
+        private DateTime _lastPurchase;
+
+        #endregion
+
+        #region Properties
+
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                if(value != _description)
+                {
+                    _description = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Description)));
+                }
+            }
+        }
+
+        public string Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if(value != _price)
+                {
+                    _price = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Price)));
+                }
+            }
+        }
+
+        public bool IsToggled
+        {
+            get
+            {
+                return _isToggled;
+            }
+            set
+            {
+                if(value != _isToggled)
+                {
+                    _isToggled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsToggled)));
+                }
+            }
+        }
+        #endregion
+
+        #region Costructor
+
         public NewProductViewModel()
         {
         }
+
+        #endregion
     }
 }
