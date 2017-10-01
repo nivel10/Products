ce6fb32119e40c698cc1e7f45e545b06d697d179
cherrera <cherrera@chejconsultor.com.ve>
2017-09-30 22:19:38 -0400

    Sincronizacion - 30/09/2017 - 10:19 PM

--- Products/Products/ViewModels/NewProductViewModel.cs
+++ Products/Products/ViewModels/NewProductViewModel.cs
@@ -5,6 +5,7 @@
     using System.Windows.Input;
     using GalaSoft.MvvmLight.Command;
     using Products.Services;
+    using Products.Models;
 
     public class NewProductViewModel : INotifyPropertyChanged
     {
@@ -16,10 +17,13 @@
         private string _price;
         private bool _isToggled;
         private DateTime _lastPurchase;
+        private string _stock;
         private string _remarks;
         private bool _isRunnig;
         private bool _isEnabled;
         private DialogService dialogService;
+        private ApiService apiService;
+        private NavigationService navigationService;
 
         #endregion
 
@@ -101,6 +105,22 @@
             }
         }
 
+        public string Stock
+        {
+            get
+            {
+                return _stock;
+            }
+            set
+            {
+                if(value != _stock)
+                { 
+                    _stock = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Stock)));
+                }
+            }
+        }
+
         public string Remarks
         {
             get
@@ -163,6 +183,7 @@
 
             //  Instancia los servicios
             dialogService = new DialogService();
+            apiService = new ApiService();
         }
 
         #endregion
@@ -185,17 +206,95 @@
 
             if(string.IsNullOrEmpty(Price))
             {
-                await dialogService.ShowMessage("Error", "You must enter a product price numeric...!!!");
-                return;
-            }
-            decimal price;
-            price = 0.0m;
-            if(!decimal.TryParse(Price, out price))
-            {
-                await dialogService.ShowMessage("Error", "You must enter a product price numeric...!!!");
+                await dialogService.ShowMessage(
+                    "Error", 
+                    "You must enter a product price...!!!");
                 return;
             }
 
+            decimal price = 0.0m;
+            if(!decimal.TryParse(Price, out price))
+            {
+                await dialogService.ShowMessage(
+                    "Error", 
+                    "You must enter a product price in numeric value...!!!");
+                return;
+            }
+
+            if(string.IsNullOrEmpty(Stock))
+            {
+                await dialogService.ShowMessage(
+                    "Error", 
+                    "You must enter a product stock...!!!");
+                return;
+            }
+
+            decimal stock = 0.0m;
+            if(!decimal.TryParse(Stock, out stock))
+            {
+                await dialogService.ShowMessage(
+                    "Error", 
+                    "You must enter a product stock in numeric value...!!!");
+                return;
+            }
+
+            if(string.IsNullOrEmpty(Remarks))
+            {
+                await dialogService.ShowMessage(
+                    "Error", 
+                    "You must enter a product remarks...!!!");
+                return;
+            }
+
+            //  Habilita el ActivityIndicator
+            SetEnabledDisable(true, false);
+
+            //  Verifica si el dispositio tiene o no acceso a internet
+            var connection = await apiService.CheckConnection();
+            if(!connection.IsSuccess)
+            {
+                SetEnabledDisable(false, true); 
+                await dialogService.ShowMessage("Error", connection.Message);
+                return;
+            }
+
+            //  Optiene una instancia del MainViewModel para extraer los datos del Token
+            var mainViewModel = MainViewModel.GetInstance();
+
+            //  Crea un objeto de tipo product
+            var product = new Product
+            {   
+                Description = Description, 
+                Image = "",
+                Price = price,
+                IsActive = IsToggled, 
+                LastPurchase = LastPurchase,
+                Stock = (double)stock,
+                Remarks = Remarks,
+            };
+
+            //  Invoca el metodo que hace el insert de datos (Api)
+            var response = await apiService.Post(
+                "http://productszuluapi.azurewebsites.net",
+                "/api",
+                "/Products",
+                mainViewModel.Token.TokenType,
+                mainViewModel.Token.AccessToken,
+                product);
+
+            //  Valida si hubo o no error en el WepAPI
+            if(!response.IsSuccess)
+            {
+                SetEnabledDisable(false, true);
+                await dialogService.ShowMessage("Error", response.Message);
+                return;
+            }
+
+            //  Mensaje de sistema
+            await dialogService.ShowMessage("Information", "Product add successful...!!!");
+
+            //  Hace el Back del NavigationService
+            await navigationService.Back();
         }
 
         /// <summary>
